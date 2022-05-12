f7d3c3f659007d765bdba0d3752925d066b09894
Lloyd Lobo <lloyd@lloydlobo.com>
2022-05-12 15:14:43 +0530

    feat: add a loop and break when user wins

--- apps/guessing-game/src/main.rs
+++ apps/guessing-game/src/main.rs
@@ -6,7 +6,6 @@ use std::io;
 // It's generating a random number between 1 and 100 and then it's comparing the user input to the random number.
 fn main() {
     println!("Guess the number");
-    println!("Please input your guess.");
 
     // Create secret number using the rand package and the thread_rng function
 
@@ -14,36 +13,44 @@ fn main() {
     let secret_number: u32 = rng.gen_range(1..101);
     println!("The secret number is: {}", secret_number);
 
-    // Creating a mutable variable called guess and assigning it the value of a new empty string.
-    let mut guess: String = String::new();
+    //  Add a loop
+    loop {
+        println!("Please input your guess.");
+        // Creating a mutable variable called guess and assigning it the value of a new empty string.
+        let mut guess: String = String::new();
 
-    // It's reading the input from the user and storing it in the variable guess.
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // It's reading the input from the user and storing it in the variable guess.
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // It's a match expression that is checking if the user input is a valid number.
-    // Trims white spaces, and Parses the input guess string to a number
-    let guess: u32 = {
-        let this = guess.trim().parse();
-        match this {
-            Ok(guess) => guess,
-            Err(error) => panic!("Please type a number: {}", error),
+        // It's a match expression that is checking if the user input is a valid number.
+        // Trims white spaces, and Parses the input guess string to a number
+        let guess: u32 = {
+            let this = guess.trim().parse();
+            match this {
+                Ok(guess) => guess,
+                Err(error) => panic!("Please type a number: {}", error),
+            }
+        };
+        println!("You guessed: {}", guess);
+
+        /* now create a match expression as err-> mismatched type*/
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("You guessed lower than: {}", secret_number),
+            Ordering::Greater => println!("You guessed higher than: {}", secret_number),
+            Ordering::Equal => {
+                println!("You win! : {}", secret_number);
+                // It's breaking the loop.
+                break;
+            }
         }
-    };
-
-    /* now create a match expression as err-> mismatched type*/
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("You guessed lower than: {}", secret_number),
-        Ordering::Equal => println!("You win! : {}", secret_number),
-        Ordering::Greater => println!("You guessed higher than: {}", secret_number),
     }
-
-    println!("You guessed: {}", guess);
 }
 
 // next - add a random number in Cargo.toml under dependencies as "rand"
 // after that run `cargo build` to install the dependency -> it shows up in Cargo.lock
 // after generating we need to compare the random secret_number to the user guess => bring in Ordering (it's an enum)
 //  Create a match expression that is checking if the user input is a valid number => Trims white spaces, and Parses the input guess string to a number
-// Now we want the user to keep guessing if they get it wrong.
+// Loop:: Now we want the user to keep guessing if they get it wrong. and add everything in the loop.
+// It never ends -> modify the Equal ordering to break the loop.
