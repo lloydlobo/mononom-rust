6a44c2144bf7846d882940c2d037f4ff2654e427
Lloyd Lobo <lloyd@lloydlobo.com>
2022-05-28 18:04:48 +0530

    test: allow dead_code for debugging

--- libs/tdd/adder/src/lib.rs
+++ libs/tdd/adder/src/lib.rs
@@ -1,11 +1,12 @@
 /// adder module
 
-// #[derive(Debug)]
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 impl Rectangle {
+    #[allow(dead_code)]
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
@@ -29,6 +30,7 @@ pub fn greeting(name: &str) -> String {
 }
 
 pub struct Guess {
+    #[allow(dead_code)]
     value: i32,
 }
 
