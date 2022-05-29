da2d5d22424ac1be696498b1ee297d00aa6b85f5
lloydlobo <lloyd@lloydlobo.com>
2022-05-29 18:15:46 +0530

    feat: display first entity a square

--- apps/bevy/invaders/src/main.rs
+++ apps/bevy/invaders/src/main.rs
@@ -12,5 +12,29 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_system)
         .run();
+}
+
+// first system - 'setup_system is a convention
+/// # Setup
+/// This system is run once at the start of the game
+/// It sets up the game state
+/// System doesn't return any value
+/// # Commands
+/// This system runs the following commands:
+/// - commands allow you to add / remove / modify entities
+fn setup_system(mut commands: Commands) {
+    // camera - 2D game
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // add rectangle entity to the scene
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(150.0, 150.0)),
+            ..Default::default() // now add_startup_system(setup_system) called once in the beginning and then later on we add commands to be called every frame
+        },
+        ..Default::default()
+    });
 }
