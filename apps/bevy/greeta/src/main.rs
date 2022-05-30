a905e95e681b4169d8527d62b83d6078b1274179
Lloyd Lobo <lloyd@lloydlobo.com>
2022-05-30 21:47:49 +0530

    feat: add player texture

--- apps/bevy/greeta/src/main.rs
+++ apps/bevy/greeta/src/main.rs
@@ -1,27 +1,39 @@
-#![allow(unused)] // silence unused warnings while exploring the code
-
 use bevy::prelude::*;
+
+// region:      --- Asset Constants
+
+const PLAYER_SPRITE: &str = "player_a_01.png";
+const PLAYER_SIZE: (f32, f32) = (144., 75.);
+// endregion:   --- Asset Constants
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .insert_resource(WindowDescriptor {
-            title: "Greeta Greets!".to_string(),
-            width: 598.0,
+            title: "Greeta Plays".to_string(),
+            width: 509.0,
             height: 676.0,
             ..Default::default()
         })
-   .add_plugins(DefaultPlugins
-                )
-       .run();
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_system)
+        .run();
 }
 
-// region: --- Archive - 1. Setup rectangle mock
+// # Setup
+fn setup_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>, // mut windows: ResMut<Windows>,
+) {
+    // set camera first
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-// sprite: Sprite {
-//     color: Color::rgb(0.25, 0.25, 0.75),
-//     custom_size: Some(Vec2::new(150.0, 150.0)),
-//     ..Default::default() // now add_startup_system(setup_system) called once in the beginning and then later on we add commands to be called every frame
-// },
+    // setup a player
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load(PLAYER_SPRITE),
+        ..Default::default()
+    });
+}
 
-// endregion: --- Archive - 1. Setup rectangle mock
+// use For dev, fasterrecompile. (dynamic link bevy framework)
+// `cargo run --release --features bevy/dynamic`
