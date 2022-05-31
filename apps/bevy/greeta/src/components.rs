ac3f23c7e8a5916723035436611de00bdea48fa6
lloydlobo <lloyd@lloydlobo.com>
2022-05-31 10:52:18 +0530

    feat: add components with velocity & player struct

---
+++ apps/bevy/greeta/src/components.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::Component;
+
+// region:      --- Common Components
+#[derive(Component)]
+pub struct Velocity {
+    pub x: f32,
+    pub y: f32,
+}
+// endregion:   --- Common Components
+
+// region:      --- Player Components
+pub struct Player;
+// endregion:   --- Player Components
