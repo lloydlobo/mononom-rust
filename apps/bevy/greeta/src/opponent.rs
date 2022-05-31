a84f5a6c54264c62c52ee6db224593fe276aacf7
lloydlobo <lloyd@lloydlobo.com>
2022-05-31 16:27:54 +0530

    style: scale down opponent_sprite

--- apps/bevy/greeta/src/opponent.rs
+++ apps/bevy/greeta/src/opponent.rs
@@ -1,4 +1,4 @@
-use crate::GameTextures;
+use crate::{GameTextures, SPRITE_SCALE};
 use bevy::prelude::*;
 
 pub struct OpponentPlugin;
@@ -12,6 +12,10 @@ impl Plugin for OpponentPlugin {
 fn opponent_spawn_system(mut commands: Commands, game_textures: Res<GameTextures>) {
     commands.spawn_bundle(SpriteBundle {
         texture: game_textures.opponent.clone(),
+        transform: Transform {
+            scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.0),
+            ..Default::default()
+        },
         ..Default::default()
     });
 }
