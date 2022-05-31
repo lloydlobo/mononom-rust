64be7fa288351c60e1258cc5956ae9d42152ad48
lloydlobo <lloyd@lloydlobo.com>
2022-05-31 11:41:30 +0530

    feat: add keyboard event system to player movement

--- apps/bevy/greeta/src/player.rs
+++ apps/bevy/greeta/src/player.rs
@@ -8,7 +8,8 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system)
-            .add_system(player_movement_system);
+            .add_system(player_movement_system)
+            .add_system(player_keyboard_event_system);
     }
 }
 
@@ -30,13 +31,30 @@ fn player_spawn_system(
             ..Default::default()
         }) // this is a component, add more with chaining insert
         .insert(Player)
-        .insert(Velocity { x: 1.0, y: 0.0 });
+        .insert(Velocity { x: 0.0, y: 0.0 }); // replace x: 1.0 with x: 0.0 as keyboard player start at rest
+}
+// Magic of Bevy: Decouple the movement from the rendering
+fn player_keyboard_event_system(
+    keyboard: Res<Input<KeyCode>>,
+    mut query: Query<&mut Velocity, With<Player>>,
+) {
+    // when you know there is a single playable entity use this method
+    if let Ok(mut velocity) = query.get_single_mut() {
+        velocity.x = if keyboard.pressed(KeyCode::Left) {
+            -1.0
+        } else if keyboard.pressed(KeyCode::Right) {
+            1.0
+        } else {
+            0.0
+        };
+    }
+    // for velocity in &mut query.iter() {}
 }
 
 fn player_movement_system(mut query: Query<(&Velocity, &mut Transform), With<Player>>) {
     for (velocity, mut transform) in query.iter_mut() {
-        let translation = &mut transform.translation;
-        translation.x += velocity.x * TIME_STEP * BASE_SPEED;
-        translation.y += velocity.y * TIME_STEP * BASE_SPEED;
+        let player_translation = &mut transform.translation;
+        player_translation.x += velocity.x * TIME_STEP * BASE_SPEED;
+        player_translation.y += velocity.y * TIME_STEP * BASE_SPEED;
     }
 }
