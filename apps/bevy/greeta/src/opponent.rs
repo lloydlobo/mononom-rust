b6924b04c834d6533f769bf4e6de9c31bfe7444a
lloydlobo <lloyd@lloydlobo.com>
2022-05-31 17:06:42 +0530

    feat: spawn the opponent randomly with rand crate

--- apps/bevy/greeta/src/opponent.rs
+++ apps/bevy/greeta/src/opponent.rs
@@ -1,5 +1,6 @@
-use crate::{GameTextures, SPRITE_SCALE};
+use crate::{GameTextures, WinSize, SPRITE_SCALE};
 use bevy::prelude::*;
+use rand::{thread_rng, Rng};
 
 pub struct OpponentPlugin;
 
@@ -9,10 +10,22 @@ impl Plugin for OpponentPlugin {
     }
 }
 
-fn opponent_spawn_system(mut commands: Commands, game_textures: Res<GameTextures>) {
+fn opponent_spawn_system(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+    win_size: Res<WinSize>,
+) {
+    // compute the x/y with random values
+    let mut rng = thread_rng();
+    let w_span = win_size.w / 2.0 - 100.0;
+    let h_span = win_size.h / 2.0 - 100.0;
+    let x = rng.gen_range(-w_span..w_span);
+    let y = rng.gen_range(-h_span..h_span);
+
     commands.spawn_bundle(SpriteBundle {
         texture: game_textures.opponent.clone(),
         transform: Transform {
+            translation: Vec3::new(x, y, 10.0),
             scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.0),
             ..Default::default()
         },
