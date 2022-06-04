503d0f3b395c1613f6aa710f32eca85a85431fe7
lloydlobo <lloyd@lloydlobo.com>
2022-06-04 16:28:57 +0530

    refactor: add plugin system to draw rectangle

--- apps/bevy/render-2d/src/main.rs
+++ apps/bevy/render-2d/src/main.rs
@@ -1,22 +1,40 @@
 // $ cargo run --bin render_2d --release --features bevy/dynamic
-
 use bevy::prelude::*;
+use std::any::type_name;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_system(bevy::input::system::exit_on_esc_system)
         .add_startup_system(setup_system)
+        .add_plugin(RectanglePlugin)
         .run();
 }
 
 fn setup_system(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+pub struct RectanglePlugin;
+
+impl Plugin for RectanglePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system_to_stage(StartupStage::PostStartup, rectangle_system);
+    }
+    fn name(&self) -> &str {
+        type_name::<Self>()
+    }
+}
+
+/// This system will draw a rectangle
+/// It will be added to the `PostStartup` stage
+fn rectangle_system(mut commands: Commands) {
     commands.spawn_bundle(SpriteBundle {
         sprite: Sprite {
-            color: Color::hsl(250.0, 0.5, 0.5),
-            custom_size: Some(Vec2::new(50.0, 50.0)),
-            ..default()
+            color: Color::hsl(156.0, 0.60, 0.60),
+            custom_size: Some(Vec2::new(200.0, 100.0)),
+            ..Default::default()
         },
-        ..default()
+        ..Default::default()
     });
 }
