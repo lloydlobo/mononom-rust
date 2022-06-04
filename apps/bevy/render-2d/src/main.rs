35ad89bcc458367bbd0657bbbee9d58b6a594ac2
lloydlobo <lloyd@lloydlobo.com>
2022-06-04 16:37:43 +0530

    refactor: move rectangle system in separate module

--- apps/bevy/render-2d/src/main.rs
+++ apps/bevy/render-2d/src/main.rs
@@ -1,6 +1,8 @@
 // $ cargo run --bin render_2d --release --features bevy/dynamic
 use bevy::prelude::*;
-use std::any::type_name;
+use rectangle::RectanglePlugin;
+
+mod rectangle;
 
 fn main() {
     App::new()
@@ -13,28 +15,4 @@ fn main() {
 
 fn setup_system(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-}
-
-pub struct RectanglePlugin;
-
-impl Plugin for RectanglePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_startup_system_to_stage(StartupStage::PostStartup, rectangle_system);
-    }
-    fn name(&self) -> &str {
-        type_name::<Self>()
-    }
-}
-
-/// This system will draw a rectangle
-/// It will be added to the `PostStartup` stage
-fn rectangle_system(mut commands: Commands) {
-    commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::hsl(156.0, 0.60, 0.60),
-            custom_size: Some(Vec2::new(200.0, 100.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
 }
