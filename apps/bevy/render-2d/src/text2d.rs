2f7821ff3aa106b5f5a09fc1d761858fea037711
lloydlobo <lloyd@lloydlobo.com>
2022-06-04 17:56:04 +0530

    feat: start text2d project

---
+++ apps/bevy/render-2d/src/text2d.rs
@@ -0,0 +1,8 @@
+// https://github.com/bevyengine/bevy/blob/latest/examples/2d/text2d.rs
+
+// #[derive(Component)]
+// struct AnimateTranslation;
+// #[derive(Component)]
+// struct AnimateRotation;
+// #[derive(Component)]
+// struct AnimateScale;
