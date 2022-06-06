e7e26407c82e7ba65604c921cb5fc945109e9cc5
lloydlobo <lloyd@lloydlobo.com>
2022-06-06 19:52:11 +0530

    docs: update reference and readme

---
+++ apps/_audio/synthesis-wavetable/README.md
@@ -0,0 +1,11 @@
+# Wavetable Synthesis
+
+Using rodio to synthesize wavetables.
+
+## Development
+
+### Run locally
+
+```shell
+cargo run --bin synthesis_wavetable --release
+```
--- apps/_audio/synthesis-wavetable/src/main.rs
+++ apps/_audio/synthesis-wavetable/src/main.rs
@@ -4,3 +4,16 @@ use rodio::{source::Source, OutputStream};
 fn main() {
     println!("Hello, world!");
 }
+
+/* REFERENCE
+    ____  ______________________  _______   ______________
+   / __ \/ ____/ ____/ ____/ __ \/ ____/ | / / ____/ ____/
+  / /_/ / __/ / /_  / __/ / /_/ / __/ /  |/ / /   / __/
+ / _, _/ /___/ __/ / /___/ _, _/ /___/ /|  / /___/ /___
+/_/ |_/_____/_/   /_____/_/ |_/_____/_/ |_/\____/_____/
+
+title: Wavetable Synthesis in Rust?? Rust Basics Tutorial [Synth #004]
+author: Jan Wilczek from WolfSound
+source: https://www.youtube.com/watch?v=v0Qp7eWVyes
+related: https://github.com/JanWilczek/wolf-sound-blog/tree/master/_rust/synthesis/wavetable
+*/
